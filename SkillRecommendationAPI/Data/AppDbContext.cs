0d66df324d33c2af78ac9eace21a2c8801e7e926
sumit singh <sumitsinghjadaun2004.com>
2026-02-09 18:20:06 +0530

    Add project files.

---
+++ SkillRecommendationAPI/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SkillRecommendationAPI.Models;
+
+namespace SkillRecommendationAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Internship> Internships { get; set; }
+    }
+}
---
+++ SkillRecommendationAPI/Models/Internship.cs
@@ -0,0 +1,11 @@
+﻿namespace SkillRecommendationAPI.Models
+{
+    public class Internship
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string RequiredSkills { get; set; } = string.Empty;
+    }
+}
